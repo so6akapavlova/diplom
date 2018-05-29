197001754fbfda3cdc1a0bcada3304108d16a55e
Alexey Lysenko <vexxx93@yandex.ru>
2018-05-29 05:44:28 +0300

    added route change controll

--- warts_converter.py
+++ warts_converter.py
@@ -7,7 +7,6 @@ from sys import argv
 def make_pings_file(warts_dir, filename):
     directory = 'rtt_files_first_hop'
     rtt_filename = filename.replace('.warts', '.txt')
-    print("{} is broken".format(filename))
     if not path.exists(directory):
         makedirs(directory)
     if path.exists(path.join(directory, rtt_filename)):
@@ -21,9 +20,17 @@ def make_pings_file(warts_dir, filename):
                 if record is None:
                     break
                 if isinstance(record, Traceroute) and record.hops:
-                    print('the hop address is {} and rtt is {}'.format(record.hops[1].address, record.hops[1].rtt))
-                    print(type(record.hops[1].rtt))
-                    outfile.write(str(record.hops[1].rtt) + '\n')
+                    try:
+                        used_hop
+                    except:
+                        used_hop = record.hops[1].address
+                        outfile.write(str(record.hops[1].rtt) + '\n')
+                    else:
+                        if record.hops[1].address == used_hop:
+                            print('the hop address is {} and rtt is {}'.format(record.hops[1].address, record.hops[1].rtt))
+                            outfile.write(str(record.hops[1].rtt) + '\n')
+                        else:
+                            print("Route has been changed from {norm} to {changed}".format(norm=used_hop, changed=record.hops[1].address))
             except Exception:
                 continue
     return 0
@@ -32,8 +39,8 @@ def make_pings_file(warts_dir, filename):
 def main():
 
     warts_dir = argv[1]
-    # warts_files = ["ebb.colgate.edu.warts"]
-    warts_files = listdir(warts_dir)
+    warts_files = ["ebb.colgate.edu.warts"]
+    # warts_files = listdir(warts_dir)
     for warts_file in warts_files:
         make_pings_file(warts_dir, warts_file)
     return 0
